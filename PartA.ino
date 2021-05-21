dcc6d6bfe71097de59643873aeddc5593fb060f7
Ikram BEN ABDEL OUAHAB <72503356+ikrambenabdelouahab@users.noreply.github.com>
2021-05-21 02:28:26 +0100

    Add files via upload

---
+++ PartA.ino
@@ -0,0 +1,120 @@
+#include <MQ2.h>
+#include "DHT.h"
+#include <SPI.h>
+#include <LoRa.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include "SSD1306.h"
+
+#define SS      18
+#define RST     14
+#define DI0     26
+#define BAND    433E6
+
+SSD1306  display(0x3c, 4, 15);
+
+#define DHTPIN 23           // Pin Digital où le capteur DHT11 est branché
+#define DHTTYPE DHT11       // DHT11 ou DHT22  
+DHT dht(DHTPIN, DHTTYPE);
+int t, h;                   //La température et l'humidité donnée par DHT11  
+
+int pin = 12;               //Pin analogic où le capteur MQ2 est branché
+int lpg, co, smoke;         //Les gas que le capteur va detecter
+
+MQ2 mq2(pin);
+
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination  = 0xAA;      // destination to send to
+//Le message envoyé par LoRa
+String msg = "";
+
+//Fonction qui affiche un texte dans l'ecran OLED
+void draw(String txt1, String txt2, String txt3) {
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    // The coordinates define the left starting point of the text
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(0, 0, txt1);
+    // The coordinates define the right end of the text
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(64, 22, txt2);
+    display.drawString(128, 33, txt3);
+    display.display();
+}
+
+
+void setup(){
+  //Allumer OLED
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH);   // while OLED is running, must set GPIO16 in high
+  
+  Serial.begin(115200);
+  while (!Serial); //If just the the basic function, must connect to a computer
+
+  // Initialising the UI will init the display too.
+  display.init();
+  display.flipScreenVertically();
+  SPI.begin(5,19,27,18);
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(BAND)) {
+    draw("Starting LoRa failed !",":(","");
+    while (1);
+  }
+  draw("LoRa Sender","LoRa Initial OK!","");
+  delay(1000);
+  
+  //Lancer les capteurs
+  dht.begin();
+  mq2.begin();
+}
+
+void loop(){
+  //Vider le message
+  msg = "";
+  
+  //Récuperer l'humidité et la temperature
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+  Serial.println("T="+String(t)+" ; H="+String(h));
+
+  //Récuperer les valeurs des Gas capter
+  lpg = mq2.readLPG();
+  co = mq2.readCO();
+  smoke = mq2.readSmoke();
+  
+  //Eviter les valers aléatoire
+  if (lpg >10000) lpg=0;
+  if (co > 10000) co=0;
+  if (smoke > 10000) smoke=0;
+
+   float* values= mq2.read(true);
+  //Serial.println(String(lpg)+","+String(co)+","+String(smoke));
+  Serial.println("LPG="+String(lpg)+" ; CO="+String(co)+" ; SMOKE="+String(smoke));
+
+  //Construction du message à envoyer 
+  msg = "" + String(t)+","+String(h)+","+String(lpg)+","+String(co)+","+String(smoke);
+  Serial.println("MESSAGE:: "+msg);
+
+  sendMessage(msg);
+  draw("LoRa Send msg N° "+String(msgCount), "" , msg );
+  
+  //Capter et envoyer les données chaque 5 secondes   
+  delay(5000);
+}
+
+//Envoie du packet LoRa
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
---
+++ PartB.ino
@@ -0,0 +1,142 @@
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include "SSD1306.h"
+
+SSD1306  display(0x3c, 4, 15);
+
+#define SS      18
+#define RST     14
+#define DI0     26
+#define BAND    433E6
+
+const char* ssid     = "raspberry";      //your network name
+const char* password = "123456789";      //your network pass
+
+//Définir l'adresse de cet équipement
+byte localAddress = 0xAA;
+
+//Message reçu par LoRa
+String incoming = "";
+
+//Fonction qui affiche un texte dans l'ecran OLED
+void draw(String txt1, String txt2, String txt3) {
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(0, 0, txt1);
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(64, 22, txt2);
+    display.drawString(128, 33, txt3);
+    display.display();
+}
+
+void setup() {
+    pinMode(16,OUTPUT);
+    digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+    delay(50); 
+    digitalWrite(16, HIGH);   // while OLED is running, must set GPIO16 in high
+  
+    Serial.begin(115200);
+    while (!Serial); //if just the the basic function, must connect to a computer
+
+    display.init();
+    display.flipScreenVertically();
+    SPI.begin(5,19,27,18);
+    LoRa.setPins(SS,RST,DI0);
+    
+    if (!LoRa.begin(BAND)) {
+      draw("Starting LoRa Receiver failed","...",":(");
+      while (1);
+    }
+    
+    draw("LoRa Receiver","Starting LoRa Success ","...");
+    
+    delay(1000);
+    // Connection au Wifi
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    // Tant que la connection n'est pas établie on reste dans la boucle
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        draw("Connecting to the Raspberry AP","..","..");
+    }
+  Serial.print("Connected to the WiFi network with IP: ");
+  Serial.println(WiFi.localIP());
+  draw("Connected to Raspberry AP","","");
+  delay(2000);
+}
+
+void loop()
+{
+    if(WiFi.status() == WL_CONNECTED){ // Vérifier la connection wifi
+      onReceive(LoRa.parsePacket());
+      Serial.println("----->>"+incoming);
+      
+       HTTPClient http;   
+
+       if (incoming!=""){
+           http.begin("http://192.168.4.1:8090/");  //Specify destination for HTTP request
+           http.addHeader("Content-Type", "text/plain");             //Specify content-type header
+         
+           int httpResponseCode = http.POST(incoming);   //Send the actual POST request
+           Serial.println(incoming);
+           http.end();  //Free resources
+       }
+    }
+    else {
+      display.clear(); 
+      display.setFont(ArialMT_Plain_10);
+      display.setTextAlignment(TEXT_ALIGN_LEFT);
+      display.drawString(0, 10, "NOT CONNECTED :(");
+      delay(3000);
+      display.display();
+    }
+    delay(10000);
+}
+
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {   // check length for error
+    //Serial.println("error: message length does not match length");
+    draw("ERROR ::","Message length does not match length","");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress ) { //&& recipient != 0xFF) {
+    draw("ERROR ::","This message is not for me.","");
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  draw("Nouveau message ! ",String(incoming),"");
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+}
+
